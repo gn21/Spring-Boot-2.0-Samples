5a51248d8d80bd754adc8e9c25a6a4efdabebb0e
y13423685271 <y362424>
2017-11-28 19:29:56 +0800

    Initial commit

---
+++ 1_preface/ch1.helloword/src/main/java/com/bee/sample/ch1/annotation/Function.java
@@ -0,0 +1,11 @@
+package com.bee.sample.ch1.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Function {
+
+    String value() default "";
+}
